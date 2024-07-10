218e736e7b6053247c147d9e5d2f8d55e4b4d365
Rakesh K <codersgyan@gmail.com>
2024-07-10 14:00:21 +0300

    get products endpoint

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,10 +1,12 @@
 import { db } from '@/lib/db/db';
 import { products } from '@/lib/db/schema';
 import { productSchema } from '@/lib/validators/productSchema';
+import { desc } from 'drizzle-orm';
 import { writeFile } from 'node:fs/promises';
 import path from 'node:path';
 
 export async function POST(request: Request) {
+    // todo: check user access.
     const data = await request.formData();
 
     let validatedData;
@@ -39,4 +41,13 @@ export async function POST(request: Request) {
     }
 
     return Response.json({ message: 'OK' }, { status: 201 });
+}
+
+export async function GET() {
+    try {
+        const allProducts = await db.select().from(products).orderBy(desc(products.id));
+        return Response.json(allProducts);
+    } catch (err) {
+        return Response.json({ message: 'Failed to fetch products' }, { status: 500 });
+    }
 }
