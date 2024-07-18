f8a0bbd7565ba26779fd06733e0f8d91b1398024
Rakesh K <codersgyan@gmail.com>
2024-07-19 10:02:44 +1200

    add key to map elements

--- src/app/admin/_components/sidebar.tsx
+++ src/app/admin/_components/sidebar.tsx
@@ -41,6 +41,7 @@ const Sidebar = () => {
                         {navItems.map((item) => {
                             return (
                                 <Link
+                                    key={item.href}
                                     href={item.href}
                                     className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
                                     <item.icon className="h-4 w-4" />
